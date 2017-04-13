8b1171eb9c88c740d6a0fc77e1c975418ee69463
P.J. Meisch <pj.meisch@sothawo.com>
2017-04-13 14:41:03 +0200

    initial commit

---
+++ src/main/kotlin/com/sothawo/taboo3/Taboo3kApplication.kt
@@ -0,0 +1,11 @@
+package com.sothawo.taboo3
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Taboo3kApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Taboo3kApplication::class.java, *args)
+}
