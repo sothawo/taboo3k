401b79a6eb3e1866e61ea036af613c08e12c9704
P.J.Meisch <pj.meisch@sothawo.com>
2017-04-16 21:31:55 +0200

    migrated the rest from java to kotlin

---
+++ src/main/kotlin/com/sothawo/taboo3/mvc/AddEditConfig.kt
@@ -0,0 +1,11 @@
+/*
+ * (c) Copyright 2017 sothawo.com
+ */
+package com.sothawo.taboo3.mvc
+
+/**
+ * a class to configure the add/edit form.
+ *
+ * @author P.J. Meisch (pj.meisch@sothawo.com)
+ */
+data class AddEditConfig(var caption: String? = null, var buttonLabel: String? = null, val mode: String? = null)
